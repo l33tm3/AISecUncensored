aae9d653f9efb8cd806e47d0f6ec6afe2d9dd312
Gustavo VEnegas <nanosoftcomunidad@gmail.com>
2024-03-25 03:24:41 -0300

    Cambios en el HTML y su estructura, ademas se mueve todo a un Index.html -> dentro de Static  | Se arreglan bugs con CSS y JS

--- main.py
+++ main.py
@@ -19,38 +19,9 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 # Ruta para la página principal
 @app.get("/", response_class=HTMLResponse)
 async def main():
-    return """
-<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Chat Interface</title>
-    <link rel="stylesheet" href="/static/styles.css"> <!-- Asegúrate de que este path sea correcto -->
-</head>
-<body>
-    <div class="chatbot-interface">
-        <div class="chatbot-content">
-            <div class="chatbot-header">
-                <h1>GustavoGPT Interfaz Beta</h1>
-                <div class="chatbot-question">
-                    ¿Cómo puedo ayudarte hoy?
-                </div>
-            </div>
-            <div class="chatbot-messages" id="chatHistory">
-                <!-- Mensajes de chat aparecerán aquí -->
-            </div>
-            <div class="chatbot-input">
-    <input type="text" id="userInput" placeholder="Escribe tu mensaje aquí..." onkeypress="handleKeypress(event)">
-    <button onclick="sendMessage()">Enviar</button>
-            </div>
-        </div>
-    </div>
-
-    <script src="/static/scripts.js"></script>
-</body>
-</html>
-    """
+    with open("static/index.html", "r") as file:
+        content = file.read()
+    return HTMLResponse(content)
 
 if __name__ == "__main__":
     import uvicorn
